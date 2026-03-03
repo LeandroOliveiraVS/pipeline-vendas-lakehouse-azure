d12b4ffa65ab3865fef3eca7d20a4be3e46df66b
Leandro Oliveira <leandro.oliveiravs@gmail.com>
2026-03-03 18:13:50 -0300

    ajuste no gold

--- notebooks/04_gold_aggregation.py
+++ notebooks/04_gold_aggregation.py
@@ -17,7 +17,8 @@ df_gold = df_silver \
         count("id").alias("total_products"),
         round(avg("price"), 2).alias("avg_price"),
         round(avg("rating_rate"), 2).alias("avg_rating"),
-        round(sum("rating_count"), 0).alias("total_reviews")
+        round(sum("rating_count"), 0).alias("total_reviews"),
+        round(sum("Total_Value"), 2).alias("revenue")
     ) \
     .orderBy("avg_rating", ascending=False)
 
